d8df75e8610e56e2ae9a37d4b6262dc20d72288e
«oleglust» <filslip@yandex.ru>
2022-07-25 13:40:41 +0300

    S1 done

--- S1/Program.cs
+++ S1/Program.cs
@@ -1,2 +1,16 @@
 ﻿int RANDOM = new Random().Next(10, 99);
+
 Console.WriteLine(RANDOM);
+
+int num1 = RANDOM / 10;
+int num2 = RANDOM % 10;
+int max = num1;
+
+Console.WriteLine($"num1 = {num1}, num2 = {num2}");
+
+if (num2 > max)
+{
+    max = num2;
+}
+
+Console.WriteLine(max);
