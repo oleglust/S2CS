9bf79e6bd1a5249f9fa76a47eee31ef1b8dc1be6
«oleglust» <filslip@yandex.ru>
2022-07-26 12:49:37 +0300

    HW10 done

---
+++ Homework(10)/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+int RANDOM = new Random().Next(100, 1000);
+
+Console.WriteLine(RANDOM);
+
+int num1 = RANDOM / 10;
+int num2 = num1 % 10;
+
+
+Console.WriteLine(num2);
+
